c290609773cfe94ee7a150ebff784bc1b764dea6
Tingting Duan <duan.tingting@yahoo.com>
2021-04-17 13:31:39 -0500

    added new toys and loop print

--- scripts/leonids-toys.js
+++ scripts/leonids-toys.js
@@ -35,4 +35,30 @@ const toys = [
     }
 ]
 
-console.log(toys)
+// Define a new toy
+const toy3 = {
+    id: 3,
+    name: "toy3",
+    maker: "Google",
+    operatingSystem: "Orange",
+    price: 750,
+    weight: 1.3
+}
+
+const toy4 = {
+    id: 4,
+    name: "toy4",
+    maker: "Apple",
+    operatingSystem: "Orange",
+    price: 100,
+    weight: 0.5
+}
+
+toys.push(toy3)
+toys.push(toy4)
+
+/* console.log(toys)*/
+
+for(let i = 0; i < toys.length; i++) {
+    console.log(toys[i].name);
+  }
