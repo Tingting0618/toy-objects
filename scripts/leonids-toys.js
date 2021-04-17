6b611ba99235548071d113dce05fe79deca34cfb
Tingting Duan <duan.tingting@yahoo.com>
2021-04-17 12:54:31 -0500

    toys array added

---
+++ scripts/leonids-toys.js
@@ -0,0 +1,38 @@
+const toy1 = {
+    id: 1,
+    name: "toy1",
+    maker: "Apple",
+    operatingSystem: "iOS",
+    price: 900,
+    weight: 1.2
+}
+
+const toy2 = {
+    id: 2,
+    name: "toy2",
+    maker: "Samsung",
+    operatingSystem: "Android",
+    price: 600,
+    weight: 1.4
+}
+
+const toys = [
+    {
+        id: 1,
+        name: "toy1",
+        maker: "Apple",
+        operatingSystem: "iOS",
+        price: 900,
+        weight: 1.2
+    },
+    {
+        id: 2,
+        name: "toy2",
+        maker: "Samsung",
+        operatingSystem: "Android",
+        price: 600,
+        weight: 1.4
+    }
+]
+
+console.log(toys)
